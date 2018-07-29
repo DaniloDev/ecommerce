b3c13d43eb7618c5d363d620719e4b9331051327
Unknown <danisn2010@gmail.com>
2018-07-29 19:03:11 -0300

    Finalizar pedido e Gerar Boleto
    Rota order e configurações de geraçãop de boleto

---
+++ vendor/DDev/php-classes/src/Model/OrderStatus.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DDev\Model;
+
+use \DDev\DB\Sql;
+use \DDev\Model;
+
+
+class OrderStatus extends Model{
+
+	const EM_ABERTO = 1;
+	const AGUARDANDO_PAGAMENTO = 2;
+	const PAGO = 3;
+	const ENTREGUE = 4;
+
+
+}
+
+?> 
